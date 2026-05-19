2fc5b07dc7025d1b159284e3ee64f6e46eaa285f
poundsmichaelscode <michealolayenikan@gmail.com>
2026-05-19 20:46:33 +0100

    javascript basics

--- JAVASCRIPT/Eloquent-Javascript/values-Types-Operators/app.js
+++ JAVASCRIPT/Eloquent-Javascript/values-Types-Operators/app.js
@@ -1,3 +1,23 @@
-//values types Operator in javascript
-s
+
+
+console.log ("this is a \n new line to write");
+
+
+a= 10
+b= 6
+
+console.log (a+b);
+
+
+console.log (' "olayenikan"+ "michael" + "olaniyi" + "is a boy"' );
+
+console.log (5 >2);
+
+console.log (2 > 5 )
+
+console.log ( 5 < 2 );
+
+console.log ( 5 > 2 );
+
+
 
