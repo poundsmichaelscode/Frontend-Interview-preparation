879740a650c57fa4d589fed3d8bf86a41fdb08e5
poundsmichaelscode <michealolayenikan@gmail.com>
2026-05-30 10:06:38 +0100

    for loop

--- JAVASCRIPT/Eloquent-Javascript/Loops/loops.js
+++ JAVASCRIPT/Eloquent-Javascript/Loops/loops.js
@@ -13,3 +13,22 @@ console.log (Triangle);
 
 
 }
+
+
+
+
+
+let chess = "";
+
+
+//the loop
+
+
+for (let i = 1; i<= 4;  i++
+)
+
+{
+
+chess  += "#### \n"}}
+
+console.log (chess);
---
+++ JAVASCRIPT/loops.js
@@ -0,0 +1,83 @@
+
+for (let i =0; i <=10; i++)
+
+{
+
+console.log(i);
+}
+
+
+
+
+
+for (let i =3; i >=  0; i--)
+
+
+
+{
+console.log(i);
+
+}
+
+
+
+console.log ("count 1-5")
+
+
+for (let count = 1; count <= 5; count++) {
+  console.log(count);
+
+
+
+}
+
+
+
+console.log("divide by 2")
+
+
+for (i =0; i<= 20 ;  i+=2)
+
+{
+console.log(i)
+};
+
+
+console.log ("by 3")
+
+
+for (i =0; i <=18 ; i +=3)
+
+
+{console.log (i)};
+
+
+
+
+
+console.log ("count down")
+
+
+for (let i =3; i>=0; i--)
+
+{ console.log(i)};
+
+
+
+
+
+
+console.log ("print stars");
+
+
+let stars ="";
+
+
+for (let star= 0;   star<=8;  star++)
+
+{ stars += "***"
+
+
+console.log (stars);
+
+}
