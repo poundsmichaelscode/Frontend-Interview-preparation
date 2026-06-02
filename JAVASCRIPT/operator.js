ee8456cce78a0e978389ec9668c737c7e34c89a5
poundsmichaelscode <michealolayenikan@gmail.com>
2026-06-02 18:08:28 +0100

    java script foundations

--- JAVASCRIPT/operator.js
+++ JAVASCRIPT/operator.js
@@ -1,1 +1,7 @@
-Operators in javascript
+Operators in javascrip
+
+
+Logical operators
+There are four logical operators in JavaScript: || (OR), && (AND), ! (NOT), ?? (Nullish Coalescing). Here we cover the first three, the ?? operator is in the next article.
+
+Although they are called “logical”, they can be applied to values of any type, not only boolean. Their result can also be of any type.`t
