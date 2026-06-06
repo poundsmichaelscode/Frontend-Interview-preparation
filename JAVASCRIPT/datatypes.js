a19cd3b83fbd8effdbb0817fdb3d4d582bacaafe
poundsmichaelscode <michealolayenikan@gmail.com>
2026-06-06 09:08:19 +0100

    pratice on strings

---
+++ JAVASCRIPT/datatypes.js
@@ -0,0 +1,48 @@
+//  ============== DATA TYPES  =======================
+
+
+
+// ++++STRING 
+
+
+const first = "Welcome";
+const second = "to the";
+const third = "jungle!";
+
+// ADD CODE BELOW
+let welcomeStatement = 'Welcome to the jungle!'
+
+
+console.log(first + second + third + welcomeStatement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ADD CODE HERE
+const name ="olawunmi" // string
+var age = 26  // number
+const loveJavaScript  = true // boolean
+let listOfComplaints = null  // null
+
+
+// Uncomment these to check your work!
+console.log('My name is ' + name);
+console.log('I am ' + age + ' years old');
+console.log('It\'s ' + loveJavaScript + ', I love JavaScript');
+console.log('The list of things I hate about JavaScript is ' + listOfComplaints);
+
+
--- JAVASCRIPT/variables.js
+++ JAVASCRIPT/variables.js
@@ -7,3 +7,27 @@
 // var
 //  
 
+
+//=====================
+
+var firstName = "Jenny";
+
+// Try un-commenting the line below
+console.log("Hi, " + firstName)
+
+// ADD CODE HERE
+let lastName = "pounds"
+
+// Uncomment the line below to check your work!
+console.log("Hi, " + firstName + " " + lastName);
+
+
+
+//controlflows
+
+//un finctionality based on a contion; a condiion must be met
+
+
+//loops
+
+//its a repeat of behavior
