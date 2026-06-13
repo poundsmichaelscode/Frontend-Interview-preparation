8d2c39b5ff2be67e245aa49803fbef687bd3dfc7
poundsmichaelscode <michealolayenikan@gmail.com>
2026-06-13 18:29:27 +0100

    functions

---
+++ JAVASCRIPT/Eloquent-Javascript/function.js
@@ -0,0 +1,26 @@
+//FUnction Declaration
+
+
+function sum(a, b){
+if {a > b
+ return "A is greater"
+
+
+
+}
+
+else{
+return "A is not Greater "
+}
+
+
+{
+
+return  10 + 20 ;
+}
+
+}
+console.log (sum());
+
+
+
