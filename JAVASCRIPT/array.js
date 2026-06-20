a7007ad066532afceebe6647a8a639601c51b562
poundsmichaelscode <michealolayenikan@gmail.com>
2026-06-20 16:07:11 +0100

    array

---
+++ JAVASCRIPT/array.js
@@ -0,0 +1,25 @@
+
+
+
+let student = ["name", "class", "subject", "grade"];
+
+
+const fruits  = ["mangoes", "oranges","apple"];
+
+
+// push is to add a value to the end of the array
+
+student.push  ("partment");
+
+fruits.push("grapes")
+
+
+console.log(student.length);
+
+console.log(fruits[1]);
+
+console.log (fruits);
+
+console.log(student);
+
+
