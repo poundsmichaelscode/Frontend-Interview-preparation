539853393cd25d291ebb6f44f4cdbd478ddf222f
poundsmichaelscode <michealolayenikan@gmail.com>
2026-07-08 23:44:30 +0100

    objectks

--- JAVASCRIPT/function.js
+++ JAVASCRIPT/function.js
@@ -1,5 +1,50 @@
 //Function in Javascript
 
 
+// function num (a, b){
+
+// console.log(a + b )
+// }
 
 
+
+// num(10, 20)
+
+
+
+// var colors = ['red', 'green', 'blue', 'yellow', 'orange']
+
+// arrayItems(colors)
+
+
+
+// function arrayItems(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(array[i])
+//     }
+// }   
+
+// arrayItems(colors)
+
+
+
+
+function letterFinder (word, match) {
+
+    for (let i = 0; i < word.length; i++) {
+      
+
+        if (word[i] === match) {
+            console.log(`Found the ${match} at ${i}`)
+        } 
+        
+        else {
+            console.log(`No match found at ${i}`)
+        }
+      
+    }
+
+}
+
+
+//   word[i] === match ? console.log(`Found the ${match} at ${i}`) : console.log(`No match found at ${i}`)
